99668cd19b433f3d2aff9c6f20507136202eaa60
Denis <9150771050@mail.ru>
2022-11-27 07:12:16 +0300

    Коммит

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
+import transport.Car;
+import transport.Car.Key;
+import transport.Car.Insurance;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        Car LadaGranta = new Car("", null, 0, null, -2015, "");
-        Car AudiA8 = new Car("Audi","A8 50 L TDI quattro", 3.0, "Чёрный", 2020, "Германия");
-        Car BMWZ8 = new Car("BMW","Z8", 3.0, "Чёрный", 2021, "Германия");
-        Car KiaSportage = new Car("Kia","Sportage", 2.4, "Красный", 2018, "Южная Корея");
-        Car HyundaiAvante = new Car("Hyundai","Avante", 1.6, "Оранжевый", 2016, "Южная Корея");
+        Car ladaGranta = new Car("", null, 0, null, -2015, "w",
+                "МКПП","седан", "х000хх000", 5, "s");
+        Car.Key ladaGrantaKey = ladaGranta.new Key("нет", "нет");
+        Car.Insurance ladaGrantaInsurance = ladaGranta.new Insurance(LocalDate.of(2003, 9,6), 5000, "123456789");
+        ladaGranta.setKey(ladaGrantaKey);
+        ladaGranta.setInsurance(ladaGrantaInsurance);
+//
+//        Car audiA8 = new Car("Audi","A8 50 L TDI quattro", 3.0, "Чёрный", 2020,
+//                "Германия", "АКПП", "", "", 5, "W");
+//        Car bmvZ8 = new Car("BMW","Z8", 3.0, "Чёрный", 2021,
+//                "Германия", "АКПП", "", "", 5, "S");
+//        Car kiaSportage = new Car("Kia","Sportage", 2.4, "Красный", 2018,
+//                "Южная Корея", "АКПП", "", "", 5, "зима");
+//        Car hyundaiAvante = new Car("Hyundai","Avante", 1.6, "Оранжевый", 2016,
+//                "Южная Корея", "АКПП", "", "22ааа22ааа", 5, "лето");
 
-        System.out.println(LadaGranta);
-        System.out.println(AudiA8);
-        System.out.println(BMWZ8);
-        System.out.println(KiaSportage);
-        System.out.println(HyundaiAvante);
+        System.out.println(ladaGranta);
+//        System.out.println(audiA8);
+//        System.out.println(bmvZ8);
+//        System.out.println(kiaSportage);
+//        System.out.println(hyundaiAvante);
     }
 }
